da61eb8c46a3e50bdce84fbda8d81533bbfefefc
BIT Student <classroom@bgit.rs>
2020-02-12 17:32:02 +0100

    ukrao od harisa

---
+++ conditions/zadaci.js
@@ -0,0 +1,185 @@
+// var a = -5;
+// var b = -2;
+// var c = -6;
+// var d = 0;
+// var e = -1;
+// if (a > b && a > c && a > d && a > e) {
+//     res=a;
+// }
+// if (b > a && b > c && b > d && b > e) {
+//     res=b;
+// }
+// if (c > a && c > b && c > d && c > e) {
+//     res=c;
+// }
+// if (d > a && d > b && d > c && d > e) {
+//     res=d;
+// }
+// if (e > a && e > b && e > c && e > d) {
+//     res=e;
+// }
+
+//  console.log(res);
+
+// var a = 0;
+// var b = -1;
+// var c = 4;
+// var lowestNumber;
+// var midleNumber;
+// var largestNumber;
+
+
+// if (a > b && a > c) {
+//     largestNumber = a;
+// }
+// if (b > a && b > c) {
+//     largestNumber = b;
+// }
+// if (c > a && c > b) {
+//     largestNumber = c;
+// }
+
+// if (a < b && a < c) {
+//     lowestNumber = a;
+// }
+// if (b < a && b < c) {
+//     lowestNumber = b;
+// }
+// if (c < a && c < b) {
+//     lowestNumber = c;
+// }
+// if (a > lowestNumber && a < largestNumber) {
+//     midleNumber = a;
+// }
+// if (b > lowestNumber && b < largestNumber) {
+//     midleNumber = b;
+// }
+// if (c > lowestNumber && c < largestNumber) {
+//     midleNumber = c;
+// }
+
+
+// console.log(largestNumber, midleNumber, lowestNumber);
+
+
+// var a = 0;
+// var b = -1;
+// var c = 4;
+
+// if (a > b && a > c) {
+//     largestNumber = a;
+//     if (b > c) {
+//         midleNumber = b;
+//         lowestNumber = c;
+//     } else {
+//         midleNumber = c;
+//         lowestNumber = b;
+//     }
+// }
+
+
+// console.log(largestNumber, midleNumber, lowestNumber);
+
+// var a = 5;
+// var isNumber = typeof a === "number";
+
+// if (isNumber) {
+//     var isPossible = a % 2;
+//     if (isPossible === 0) {
+//         console.log("Number is dividible by 2");
+
+//     }
+//     else {
+//         console.log("X");
+
+//     }
+
+// }
+// else {
+//     console.log("Your input is not a namber");
+// }
+
+
+
+
+// var a = 10;
+// var b = 2;
+
+// if (a > b) {
+//     largestNumber = a;
+// } else {
+//     largestNumber = b;
+// }
+// console.log("Largest number is " + largestNumber);
+
+// var tempC = 60;
+
+// var tempInF = (9 * tempC / 5) + 32;
+// console.log("That is " + tempInF);
+
+// var a = 13;
+// var sampleInput = 32;
+// if (sampleInput > a) {
+//     var res = (sampleInput - a) * 2
+// } else {
+//     var res = a - sampleInput
+
+// }
+
+// console.log(res);
+// Tast 9
+// var a = 3;
+// var b = 2;
+// var c;
+// var areSame = a === b;
+// if (areSame) {
+//     c = (a + b) * 3;
+
+// } else {
+//     c = a + b;
+// }
+// console.log(c);
+// var a = 50;
+// var b = 1;
+// var result;
+// if (a === 50 || b === 50 || a + b === 50) {
+//     console.log("true")
+// } else {
+//     console.log("----");
+// }
+// Task 10
+// var a = 104;
+// if (a >= 20 && a <= 400) {
+//     if (a <= 100) {
+//         res = "20<=>100";
+//     }
+//     else {
+//         res = "100<=>400"
+//     }
+
+
+// } else {
+//     res = "-"
+// }
+
+// console.log(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ loops/swich.js
@@ -0,0 +1,255 @@
+
+// var day = 17;
+// var res;
+
+// switch (day) {
+//     case 1:
+//         res = "monday";
+//         break;
+//     case 2:
+//         res = "tuesday";
+//         break;
+//     case 3:
+//         res = "wednesday";
+//         break;
+//     case 4:
+//         res = "thursday";
+//         break;
+//     case 5:
+//         res = "friday";
+//         break;
+//     case 6:
+//         res = "saturday";
+//     case 7:
+//         res = "sunday";
+//         break;
+//     default:
+//         res = 'I don\'t know';
+//         break;
+// }
+// console.log(res);
+
+// This is swich exersices
+
+// var day = 17;
+// var res;
+
+// switch (day) {
+//     case 1:
+//         res = "monday";
+//         break;
+//     case 2:
+//         res = "tuesday";
+//         break;
+//     case 3:
+//         res = "wednesday";
+//         break;
+//     case 4:
+//         res = "thursday";
+//         break;
+//     case 5:
+//         res = "friday";
+//         break;
+//     case 6:
+//         res = "saturday";
+//     case 7:
+//         res = "sunday";
+//         break;
+//     default:
+//         res = 'Input must be a number between 1 and 7';
+//         break;
+// }
+// console.log(res);
+
+// Week day 
+
+
+// var day = 1;
+// var res;
+
+// switch (day) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         res = "Its weekday";
+//         break;
+//     case 6:
+//     case 7:
+//         res = "It's a weekend";
+//         break;
+//     default:
+//         res = 'Input must be a number between 1 and 7';
+//         break;
+// }
+// console.log(res);
+
+//Month
+
+// var month = 3;
+// var res;
+
+// switch (month) {
+//     case 1:
+//         res = "January";
+//         break;
+//     case 2:
+//         res = "February";
+//         break;
+//     case 3:
+//         res = "March";
+//         break;
+//     case 4:
+//         res = "April";
+//         break;
+//     case 5:
+//         res = "May";
+//         break;
+//     case 6:
+//         res = "June";
+//         break;
+//     case 7:
+//         res = "July";
+//         break;
+//     case 8:
+//         res = "August";
+//         break;
+//     case 9:
+//         res = "September";
+//         break;
+//     case 10:
+//         res = "October";
+//         break;
+//     case 11:
+//         res = "November";
+//         break;
+//     case 12:
+//         res = "December";
+//         break;
+//     default:
+//         res = "Input must be a number between 1 and 12."
+//         break;
+// }
+// console.log(res);
+
+// Season for monts
+
+// var month = 12;
+// var res;
+// switch (month) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         res = "Winter";
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         res = "Spring";
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         res = "Summer";
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         res = "Autumn"
+//         break;
+//     default:
+//         res = "Input must be a number between 1 and 12";
+//         break;
+// }
+// console.log(res);
+
+// Grades sistem in swich
+
+// var grade = "Q";
+// var res;
+// switch (grade) {
+//     case "A":
+//         res = "Good job";
+//         break;
+//     case "B":
+//         res = "Pretty good";
+//         break;
+//     case "C":
+//         res = "Passed";
+//         break;
+//     case "D":
+//         res = "Not so good";
+//         break;
+//     case "F":
+//         res = "Failed";
+//         break;
+//     default:
+//         res = "Unknown grade";
+//         break;
+// }
+// console.log(res);
+
+// Contries citys
+
+// var city = "Paris";
+// var res;
+
+// switch (city) {
+//     case "Belgrade":
+//     case "Smederevo":
+//     case "Novi Pazar":
+//         res = "Serbia";
+//         break;
+//     case "New York":
+//     case "Chicago":
+//     case "Las Vegas":
+//         res = "USA";
+//         break;
+//     case "Istanbul":
+//     case "Ankara":
+//     case "Izmir":
+//         res = "Turkey";
+//         break;
+//     case "Berlin":
+//     case "Frankfurt":
+//     case "Dortmund":
+//         res = "Germany";
+//         break;
+//     case "Paris":
+//     case "Lyon":
+//     case "Nice":
+//         res = "France";
+//         break;
+//     default:
+//         res = "Please choose a different city";
+//         break;
+// }
+// console.log(res);
+
+// Array 
+
+// var months = ["January", " February", "March", "April", "may", "June", "July", "August", "September", "October", "November", "December"];
+
+// console.log(months[5], months[9], months[0]);
+
+//var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+//console.log(days[6]);
+
+// var a = [2, -4, 5, -2, -11];
+
+// console.log(a[1], a[3], a[4]);
+
+// var s = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+// console.log(s[1], s[4], s[5], s[6], s[8]);
+
+// var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// console.log(a[0][3]);
+// console.log(a[2]);
+// console.log(a[2][1]);
+
+
+
