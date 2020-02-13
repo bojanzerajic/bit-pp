6a0935f6585816ef2fd590c1ffb42952ef5041f8
BIT Student <classroom@bgit.rs>
2020-02-13 18:04:20 +0100

    Loops

---
+++ loops while/while.js
@@ -0,0 +1,74 @@
+// var a = 1;
+// var maxNumber = 10;
+
+// while (a <= maxNumber) {
+//     console.log(a);
+//     a = a + 1;
+//
+
+// for (var d = 10; d > 0; d--) {
+//     console.log("Number is:", d);
+//     for (var a = 0; a <= 10; a++) {
+//         console.log("Number is:", a);
+//     }
+
+// }
+
+// Task 1 odd even if in for
+
+// var res;
+
+// for (var a = 0; a <= 15; a++) {
+//     if (a % 2 === 0) {
+//         console.log("even :", a);
+//     } else {
+//         console.log("odd :", a);
+//     }
+// }
+
+// Task 2 
+
+
+// var n;
+// var res;
+
+// for (n = 1; n <= 1000; n++) {
+//     if (n % 3 === 0 && n % 5 === 0) {
+//         res = n + res
+//     }
+// }
+
+// console.log(res);
+
+//Task 3
+
+// var a = [2, 5, 8, 7];
+// var sum = 0;
+// var product = 1;
+
+// for (var i = 0; i < a.length; i++) {
+//     sum = a[i] + sum
+//     product = a[i] * product
+// }
+// console.log(sum, product);
+
+// Task 4
+
+// var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+// var string = "";
+
+// for (var i = 0; i < x.length; i++) {
+//     string = string + x[i]
+// }
+
+// console.log(string);
+
+//Task 5 
+
+// var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27]];
+// res = 0;
+// for (var i = 0; i < a.length; i++) {
+//     res = a[i] + res;
+// }
+
+// console.log(res);
