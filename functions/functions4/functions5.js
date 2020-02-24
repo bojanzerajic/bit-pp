6e8ab16cc41cddb47d44b94b3db735a54af48e55
unknown <Bojan Zerajic>
2020-02-24 21:29:08 +0100

    functions 5 exercise

--- functions/functions4/functions5.js
+++ functions/functions4/functions5.js
@@ -138,6 +138,67 @@
 
 ////////////////////////////////////////////// TASK 10 //////////////////////////////////////////////////
 
+// Write a program that checks if the entered number is a prime number (i.e. divisible only by 1 and by itself).
+// Input:  17    | 15
+// Output: true  | false
+
+// var isItPrimeNumber = function (number) {
+//     var primeOrNot = true;
+//     for (var i = 2; i < number; i++) {
+//         if (number % i === 0) {
+//             primeOrNot = false;
+//         }
+//     }
+//     return primeOrNot;
+// }
+// console.log(isItPrimeNumber(12));
+
+////////////////////////////////////////////// TASK 11 //////////////////////////////////////////////////
+
+// Check if a given string is a palindrome (spaces are ignored).
+// Input:  eye  | Geek  | a nut for a jar of tuna
+// Output: true | false | true
+
+// var isStringPalindrome = function (string) {
+//     var newString = "";
+//     var invertString = "";
+//     var result = false;
+//     for (var i = 0; i < string.length; i++) {
+//         if (string[i] === " ") {
+//             continue;
+//         }
+//         newString += string[i];
+//     }
+//     for (var j = newString.length - 1; j >= 0; j--) {
+//         invertString += newString[j];
+//     }
+//      if (newString === invertString) {
+//          result = true;
+//      }
+//      return result;
+// }
+// console.log(isStringPalindrome("eye"));
+
+////////////////////////////////// TASK 12 ///////////////////////////////////////////
+
+// Write a program that calculates the greatest common divisor of two integers. Note: The greatest common divisor of two non-zero integers is the greatest positive number that divides both numbers with no remainder.
+// Input:  192 42 | 81 9
+// Output: 6      | 9
+
+// var greatestCommonDivisor = function (numberOne, numberTwo) {
+//     var result = 1;
+//     var biggerNumber;
+//     numberOne > numberTwo ? biggerNumber = numberOne : biggerNumber = numberTwo;
+//     for (var i = 2; i <= biggerNumber; i++) {
+//         if (numberOne % i === 0 && numberTwo % i === 0) {
+//             result = i;
+//         }
+//     }
+//     return result;
+// }
+// console.log(greatestCommonDivisor(112, 56));
+
+
 
 
 
